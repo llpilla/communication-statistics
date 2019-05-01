f5a2caa2ee73d475c9cf09f02eb0d945b1a59581
Laércio LIMA PILLA <pilla@lri.fr>
2019-05-01 11:44:53 +0200

    Docstring update to follow numpydoc guidelines

--- commstats.py
+++ commstats.py
@@ -9,8 +9,28 @@ import numpy as np
 
 class CommunicationStatistics(object):
     """
-    Class to compute multiple communication metrics/statistics over a given communication matrix
-    Statistics considered:
+    Computes multiple communication metrics/statistics over a given communication matrix.
+
+    Parameters
+    ----------
+    csv_file : string
+        Name of the CSV file containing the communication costs matrix.
+
+    Attributes
+    ----------
+    costs : np.ndarray
+        Communication costs matrix.
+
+    Raises
+    ------
+    ValueError
+        If the matrix contains NaNs, negative values, or if the matrix is not square.
+    ZeroDivisionError
+        If the matrix contains any rows with zeros only.
+
+    Notes
+    -----
+    Communication statistics considered:
     - CH: communication heterogeneity (Diener et al, Performance Evaluation 2015)
     - CA: communication amount (Diener et al, Performance Evaluation 2015)
     - CB: communication balance (Diener et al, Euro-Par 2015)
@@ -26,22 +46,21 @@ class CommunicationStatistics(object):
         C(x,y): value of position x,y of C
         sum_i[1..3]X(i) = X(1)+X(2)+X(3)
 
-    Referenced papers:
+    References
+    ----------
     - Diener et al, Performance Evaluation 2015): Matthias Diener, Eduardo HM Cruz, Laercio L.
     Pilla, Fabrice Dupros, and Philippe OA Navaux. "Characterizing communication and page usage
-    of parallel applications for thread and data mapping." Performance Evaluation 88 (2015):
-    18-36.
+    of parallel applications for thread and data mapping." Performance Evaluation 88 (2015): 18-36.
     - Diener et al, Euro-Par 2015: Matthias Diener, Eduardo HM Cruz, Marco AZ Alves, Mohammad S.
-    Alhakeem, Philippe OA Navaux, and Hans-Ulrich Heiss. "Locality and balance for communication-
-    aware thread mapping in multicore systems." In European Conference on Parallel Processing,
-    pp. 196-208. Springer, Berlin, Heidelberg, 2015.
+    Alhakeem, Philippe OA Navaux, and Hans-Ulrich Heiss. "Locality and balance for
+    communication-aware thread mapping in multicore systems." In European Conference on
+    Parallel Processing, pp. 196-208. Springer, Berlin, Heidelberg, 2015.
     - Bordage and Jeannot, CCGrid 2018: Cyril Bordage, and Emmanuel Jeannot. "Process Affinity,
     Metrics and Impact on Performance: an Empirical Study." In 2018 18th IEEE/ACM International
     Symposium on Cluster, Cloud and Grid Computing (CCGRID), pp. 523-532. IEEE, 2018.
     """
 
     def __init__(self, csv_file):
-        """Reads the communication matrix. Takes a CSV file as argument."""
         self.costs = np.genfromtxt(csv_file, delimiter=',')
         # Checking if the matrix has any NaNs
         if np.isnan(self.costs).any():
@@ -67,6 +86,14 @@ class CommunicationStatistics(object):
     def communication_heterogeneity(self):
         """
         Computes the communication heterogeneity (CH) of a matrix.
+
+        Returns
+        -------
+        np.float64
+            Communication heterogeneity value.
+
+        Notes
+        -----
         Given M = 100*C/max(C) (normalized matrix),
         CH = (sum_i sum_j ( sum_k(M(i,k))/n - M(i,j) )^2 )/n^2
         It can also be seen as the sum of the variances for each row
@@ -79,12 +106,26 @@ class CommunicationStatistics(object):
         return stats
 
     def ch(self):   # pylint: disable=invalid-name
-        """Check communicationHeterogeneity for information."""
+        """
+        Short-hand for communication_heterogeneity.
+
+        See Also
+        --------
+        communication_heterogeneity : Computes the communication heterogeneity of a matrix.
+        """
         return self.communication_heterogeneity()
 
     def communication_amount(self):
         """
         Computes the communication amount (CA) of a matrix.
+
+        Returns
+        -------
+        np.float64
+            Communication amount value.
+
+        Notes
+        -----
         CA = sum(C)/n^2
         """
         dim = self.costs.shape[0]
@@ -92,12 +133,26 @@ class CommunicationStatistics(object):
         return stats
 
     def ca(self):   # pylint: disable=invalid-name
-        """Check communicationAmount for information."""
+        """
+        Short-hand for communication_amount.
+
+        See Also
+        --------
+        communication_amount : Computes the communication amount of a matrix.
+        """
         return self.communication_amount()
 
     def communication_balance(self):
         """
         Computes the communication balance (CB) of a matrix.
+
+        Returns
+        -------
+        np.float64
+            Communication balance value.
+
+        Notes
+        -----
         Given T(i) = sum(C(i)),
         CB = (max(T)/(sum_i(T(i))/n) - 1)*100
         """
@@ -107,12 +162,26 @@ class CommunicationStatistics(object):
         return stats
 
     def cb(self):   # pylint: disable=invalid-name
-        """Check communicationBalance for information."""
+        """
+        Short-hand for communication_balance.
+
+        See Also
+        --------
+        communication_balance : Computes the communication balance of a matrix.
+        """
         return self.communication_balance()
 
     def communication_centrality(self):
         """
         Computes the communication centrality (CC) of a matrix.
+
+        Returns
+        -------
+        np.float64
+            Communication centrality value.
+
+        Notes
+        -----
         Consider, for simplicity, that C(i,j) = 0 if j<0 or j>=n.
         Given a radius r>0 around i such that at least half of the communication
         costs of process i lie within it,
@@ -140,12 +209,26 @@ class CommunicationStatistics(object):
         return stats
 
     def cc(self):   # pylint: disable=invalid-name
-        """Check communicationCentrality for information."""
+        """
+        Short-hand for communication_centrality.
+
+        See Also
+        --------
+        communication_centrality : Computes the communication centrality of a matrix.
+        """
         return self.communication_centrality()
 
     def neighbor_communication_fraction(self):
         """
         Computes the neighbor communication fraction (NBC) of a matrix.
+
+        Returns
+        -------
+        np.float64
+            Neighbor communication fraction value.
+
+        Notes
+        -----
         Consider, for simplicity, that C(i,j) = 0 if j<0 or j>=n.
         NBC = 1 - sum_i( C(i,i-1)+C(i,i+1) )/sum(C)
         """
@@ -158,13 +241,36 @@ class CommunicationStatistics(object):
         return stats
 
     def nbc(self):   # pylint: disable=invalid-name
-        """Check neighborCommunicationFraction for information."""
+        """
+        Short-hand for neighbor_communication_fraction.
+
+        See Also
+        --------
+        neighbor_communication_fraction : Computes the neighbor communication fraction of a matrix.
+        """
         return self.neighbor_communication_fraction()
 
     def split_fraction(self, k=2):
         """
         Computes the split fraction for k (SP(k)) of a matrix.
-        Takes k as a parameter (default k=2)
+
+        Parameters
+        ----------
+        k : int, optional
+            Size of the sub-matrix considered (default = 2).
+
+        Returns
+        -------
+        np.float64
+            Split fraction value.
+
+        Raises
+        ------
+        ValueError
+            If the value of k is smaller than 1.
+
+        Notes
+        -----
         The split fraction is the amount of communication that is done
         around blocks of k * k processes.
         SP(k) = 1 - sum_s[..n/k-1]sum_l[0..k]sum_m[0..k]C(s*k+l,s*k+m)/sum(C)
@@ -182,12 +288,26 @@ class CommunicationStatistics(object):
         return stats
 
     def sp(self, k=2):   # pylint: disable=invalid-name
-        """Check splitFraction for information."""
+        """
+        Short-hand for split_fraction.
+
+        See Also
+        --------
+        split_fraction : Computes the split fraction of a matrix.
+        """
         return self.split_fraction(k)
 
     def communication_heterogeneity_v2(self):
         """
-        Computes the communication heterogeneity (CH) of a matrix.
+        Computes the communication heterogeneity (CH) of a matrix with a different formula.
+
+        Returns
+        -------
+        np.float64
+            Communication heterogeneity value.
+
+        Notes
+        -----
         Given M = C/max(C) (normalized matrix),
         CH = (sum_i sum_j ( sum_k(M(i,k))/n - M(i,j) )^2 )/n^2
         It can also be seen as the sum of the variances for each row
@@ -200,12 +320,26 @@ class CommunicationStatistics(object):
         return stats
 
     def ch_v2(self):   # pylint: disable=invalid-name
-        """Check communicationHeterogeneity for information."""
+        """
+        Short-hand for communication_heterogeneity_v2.
+
+        See Also
+        --------
+        communication_heterogeneity_v2 : Computes the communication heterogeneity of a matrix.
+        """
         return self.communication_heterogeneity_v2()
 
     def communication_balance_v2(self):
         """
-        Computes the communication balance (CB) of a matrix.
+        Computes the communication balance (CB) of a matrix with a different formula.
+
+        Returns
+        -------
+        np.float64
+            Communication balance value.
+
+        Notes
+        -----
         Given T(i) = sum(C(i)),
         CB = 1 - (sum_i(T(i)/n)/max(T)
         """
@@ -215,5 +349,11 @@ class CommunicationStatistics(object):
         return stats
 
     def cb_v2(self):   # pylint: disable=invalid-name
-        """Check communicationBalance for information."""
+        """
+        Short-hand for communication_balance_v2.
+
+        See Also
+        --------
+        communication_balance_v2 : Computes the communication balance of a matrix.
+        """
         return self.communication_balance_v2()
